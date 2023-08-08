e008a8ce9787948d30017d0c87722870764437a4
Banou <me@banou.dev>
2023-08-08 02:07:34 +0200

    feat: ✨animetosho & anizip

---
+++ src/origins/animetosho.ts
@@ -0,0 +1,8 @@
+
+export default {
+  origin: 'animetosho.org',
+  cache: {
+    ttl: 1000 * 60 * 60 * 1
+  },
+  throttles: []
+}
---
+++ src/origins/anizip.ts
@@ -0,0 +1,8 @@
+
+export default {
+  origin: 'api.ani.zip',
+  cache: {
+    ttl: 1000 * 60 * 60 * 1
+  },
+  throttles: []
+}
